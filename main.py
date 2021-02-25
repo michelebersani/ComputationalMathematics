4b6186c4e8ec1e92d2854a29e75876283beb4cb6
FeLusiani <felusiani@gmail.com>
2021-02-25 01:38:09 +0100

    better print

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from LBFGS import LBFGS_2D, LBFGS
 import ackley
 import logging
+import numpy as np
 
 if __name__ == '__main__':
     # set level to WARNING to avoid printing INFOs
     logging.basicConfig(level='INFO')
 
     solver = LBFGS()
-    status = solver.solve(ackley, [2,3])
+    status = solver.solve(ackley, [2,3.1])
+    print('')
     print(f'Exited with status: {status}')
-    print("Minimum function value found is:")
-    print(solver.f_value)
+    print(f"Minimum function value found is: {solver.f_value}")
     print(f"f evaluations: {solver.feval}")
+    print(f"Last gradient was: {solver.g}")
+    print('')
 
-    LBFGS_2D(ackley, [2,3])
+    #LBFGS_2D(ackley, [2,3])
