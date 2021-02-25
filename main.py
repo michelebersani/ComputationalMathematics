1e2d795e143068d559db8431ab1a25e13ce4e96d
FeLusiani <felusiani@gmail.com>
2021-02-25 01:32:58 +0100

    added LBFGS and LBFGS 2D

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-from LBFGS import LBFGS
+from LBFGS import LBFGS_2D, LBFGS
 import ackley
+import logging
 
 if __name__ == '__main__':
-    LBFGS(ackley, [4,3])
+    # set level to WARNING to avoid printing INFOs
+    logging.basicConfig(level='INFO')
+
+    solver = LBFGS()
+    status = solver.solve(ackley, [2,3])
+    print(f'Exited with status: {status}')
+    print("Minimum function value found is:")
+    print(solver.f_value)
+    print(f"f evaluations: {solver.feval}")
+
+    LBFGS_2D(ackley, [2,3])
