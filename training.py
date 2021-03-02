9ff2767f2639a4cbbc6e2c931cd926ba59965270
FeLusiani <felusiani@gmail.com>
2021-03-02 12:32:29 +0100

    modified training.py

--- training.py
+++ training.py
@@ -10,9 +10,9 @@ from NN.utility import batch_train, batch_out, Model_Wrapper
 
 from LBFGS import LBFGS
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     data = pd.read_csv(shuffled_csv, index_col=0).to_numpy()
-    data = data[:500,:]
+    data = data[:500, :]
     n_samples = data.shape[0]
     X_data = data[:, :10]
     Y_data = data[:, 10:]
@@ -20,29 +20,32 @@ if __name__ == '__main__':
     Y_scaler = StandardScaler()
     Y_scaled = Y_scaler.fit_transform(Y_data)
 
-    np.random.seed(10)
-    model = NN_model([10, 20, 20, 2], Sigmoid, MSE)
+    # np.random.seed(10)
+    model = NN_model([10, 50, 50, 2], Sigmoid, MSE)
     model.init_weights()
 
-    # set level to WARNING to avoid printing INFOs
-    logging.basicConfig(level='INFO')
+    reg_loss = L2_reg(1e-2)
 
-    solver = LBFGS(eps=1e-5,max_feval=500,m2=0.7, m1=0.1)
-    f = Model_Wrapper(model, X_data, Y_scaled)
+    # set level to WARNING to avoid printing INFOs
+    logging.basicConfig(level="INFO")
+
+    solver = LBFGS(max_feval=200000, M=1000)
+    f = Model_Wrapper(model, X_data, Y_scaled, reg_loss)
     x = model.Weights
-    status = solver.solve(f,x)
-    print('')
-    print(f'Exited with status: {status}')
+    status = solver.solve(f, x)
+    print("")
+    print(f"Exited with status: {status}")
     print(f"f evaluations: {solver.feval}")
-    print('')
+    print(f"g norm: {np.linalg.norm(solver.g)}")
+    print("")
 
     Y_out = batch_out(model, X_data)
     Y_out = Y_scaler.inverse_transform(Y_out)
-    plt.scatter(Y_data[:,0], Y_data[:,1])
-    plt.scatter(Y_out[:,0], Y_out[:,1])
+    plt.scatter(Y_data[:, 0], Y_data[:, 1])
+    plt.scatter(Y_out[:, 0], Y_out[:, 1])
 
-    print('MEE is:')
+    print("MEE is:")
     mee = 0
     for y1, y2 in zip(Y_data, Y_out):
         mee += np.linalg.norm(y1 - y2)
-    print(mee/n_samples)
+    print(mee / n_samples)
