9724840639aa4ffa54841a59b778a4075edfc6d8
FeLusiani <felusiani@gmail.com>
2021-03-05 12:16:15 +0100

    type hinting fix

--- NN/utility.py
+++ NN/utility.py
@@ -1,22 +1,26 @@
 import numpy as np
 from .NN import NN_model
-from .Functions import L2_reg
+from .Functions import RegLossFunction
 import multiprocessing
 from multiprocessing import Pool
 
-class Model_Wrapper():
-    def __init__(self, model: NN_model, X: np.ndarray, Y: np.ndarray, reg_loss:L2_reg):
+
+class Model_Wrapper:
+    def __init__(
+        self, model: NN_model, X: np.ndarray, Y: np.ndarray, reg_loss: RegLossFunction
+    ):
         self.model = model
         self.X = X
         self.Y = Y
         self.reg_loss = reg_loss
 
-    def function(self, weights:np.ndarray):
+    def function(self, weights: np.ndarray):
         self.model.Weights = weights
         self.model.zero_grad()
         loss = batch_train(self.model, self.X, self.Y) + self.reg_loss(weights)
         gradient = self.model.get_grad() + self.reg_loss.grad
         return loss, gradient
+
 
 def batch_train(model: NN_model, X: np.ndarray, Y: np.ndarray):
     """Runs the input `model` over the dataset `(X,Y)`,
