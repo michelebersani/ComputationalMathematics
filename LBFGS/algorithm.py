873f3d2425be2af1ee4de43ed69da9bea8f99e23
FeLusiani <felusiani@gmail.com>
2021-03-08 17:11:59 +0100

    LBFGS is not directly callable anymore

--- LBFGS/algorithm.py
+++ LBFGS/algorithm.py
@@ -60,7 +60,9 @@ class LBFGS:
         self.new_x = None
         self.new_g = None
 
-    def __call__(self, f, x):
+        self.status = None
+
+    def solve(self, f, x):
         self.f = f
         self.x = x
         self.f_value, self.g = self.f(self.x)
