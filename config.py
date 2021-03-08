fdd859d32f3f671cf81fbfdd317546952f80d6bf
FeLusiani <felusiani@gmail.com>
2021-03-08 16:06:35 +0100

    added some paths

--- config.py
+++ config.py
@@ -2,4 +2,6 @@ from pathlib import Path
 
 path_prj        = Path(__file__).parents[0]
 path_data       = path_prj  / Path('./data')
+path_exps       = path_prj / Path('./experiments')
 shuffled_csv    = path_data / Path('./shuffled_cup.csv')
+experiments_csv = path_data / Path('./experiments.csv')
